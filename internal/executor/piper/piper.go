39c43d9b40af18c8b828ba585f705e7c0091be54
Nikolay Edigaryev <edigaryev@gmail.com>
2021-09-12 20:44:47 +0300

    ShellCommandsAndWait: don't wait for children processes

---
+++ internal/executor/piper/piper.go
@@ -0,0 +1,55 @@
+package piper
+
+import (
+	"errors"
+	"io"
+	"os"
+	"time"
+)
+
+type Piper struct {
+	r, w *os.File
+	errChan chan error
+}
+
+func New(output io.Writer) (*Piper, error) {
+	r, w, err := os.Pipe()
+	if err != nil {
+		return nil, err
+	}
+
+	piper := &Piper{
+		r: r,
+		w: w,
+		errChan: make(chan error),
+	}
+
+	go func() {
+		_, err := io.Copy(output, r)
+		piper.errChan <- err
+	}()
+
+	return piper, nil
+}
+
+func (piper *Piper) Input() *os.File {
+	return piper.w
+}
+
+func (piper *Piper) Close() (result error) {
+	result = piper.r.SetReadDeadline(time.Now().Add(time.Millisecond * 100))
+
+	if err := <-piper.errChan; err != nil && result == nil {
+		result = err
+	}
+
+	if err := piper.r.Close(); err != nil && result == nil {
+		result = err
+	}
+
+	if err := piper.w.Close(); err != nil && result == nil && !errors.Is(err, os.ErrClosed) {
+		result = err
+	}
+
+	return result
+}
