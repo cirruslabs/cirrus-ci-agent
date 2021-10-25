fc27918ea32289b41d07c92d07c87663f7e3bc7c
Nikolay Edigaryev <edigaryev@gmail.com>
2021-10-25 20:06:27 +0300

    TestMetrics: wait 4.5 seconds (instead of 5) for 4 metrics

--- internal/executor/metrics/metrics_test.go
+++ internal/executor/metrics/metrics_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestMetrics(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second + 500*time.Millisecond)
 	defer cancel()
 
 	resultChan, errChan := metrics.Run(ctx, nil)
