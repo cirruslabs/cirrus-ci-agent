4d1d2ad10aa54ab0fd293f330a17c477da243e11
Nikolay Edigaryev <edigaryev@gmail.com>
2023-05-29 12:54:25 +0400

    Introduce VAULT_CACHED[...] to explicitly ask for Vault secret caching

--- internal/executor/vaultunboxer/vaultunboxer_test.go
+++ internal/executor/vaultunboxer/vaultunboxer_test.go
@@ -1,6 +1,3 @@
-//go:build linux
-// +build linux
-
 package vaultunboxer_test
 
 import (
@@ -11,6 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	"testing"
+	"time"
 )
 
 func TestVault(t *testing.T) {
@@ -59,4 +57,85 @@ func TestVault(t *testing.T) {
 	secretValue, err := vaultunboxer.New(client).Unbox(ctx, selector)
 	require.NoError(t, err)
 	require.Equal(t, secretKeyValue, secretValue)
+}
+
+func TestVaultUseCache(t *testing.T) {
+	ctx := context.Background()
+
+	var vaultToken = uuid.New().String()
+
+	// Create and start the HashiCorp's Vault container
+	request := testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image:        "vault:latest",
+			ExposedPorts: []string{"8200/tcp"},
+			Env: map[string]string{
+				"VAULT_DEV_ROOT_TOKEN_ID": vaultToken,
+			},
+		},
+		Started: true,
+	}
+	container, err := testcontainers.GenericContainer(ctx, request)
+	require.NoError(t, err)
+	defer container.Terminate(ctx)
+
+	// Create demo data with an OTP token
+	vaultURL, err := container.Endpoint(ctx, "http")
+	require.NoError(t, err)
+
+	client, err := vault.NewClient(vault.DefaultConfig())
+	require.NoError(t, err)
+
+	require.NoError(t, client.SetAddress(vaultURL))
+	client.SetToken(vaultToken)
+
+	require.NoError(t, client.Sys().MountWithContext(ctx, "totp", &vault.MountInput{Type: "totp"}))
+
+	_, err = client.Logical().WriteWithContext(ctx, "/totp/keys/test", map[string]interface{}{
+		"key":    "Y64VEVMBTSXCYIWRSHRNDZW62MPGVU2G",
+		"period": "1",
+	})
+	require.NoError(t, err)
+
+	// Prepare
+	vaultUnboxer := vaultunboxer.New(client)
+
+	otpWithoutCache, err := vaultunboxer.NewBoxedValue("VAULT[totp/code/test code]")
+	require.NoError(t, err)
+
+	otpWithCache, err := vaultunboxer.NewBoxedValue("VAULT_CACHED[totp/code/test code]")
+	require.NoError(t, err)
+
+	// Make sure VAULT_CACHED[...] falls back to VAULT[...] in case no cache entry is found
+	first, err := vaultUnboxer.Unbox(ctx, otpWithCache)
+	require.NoError(t, err)
+
+	// Wait for the OTP code to rotate
+	time.Sleep(time.Second * 5)
+
+	// Make sure VAULT_CACHED[...] uses cache
+	second, err := vaultUnboxer.Unbox(ctx, otpWithCache)
+	require.NoError(t, err)
+
+	require.EqualValues(t, first, second)
+
+	// Make sure VAULT[...] busts the cache
+	third, err := vaultUnboxer.Unbox(ctx, otpWithoutCache)
+	require.NoError(t, err)
+
+	require.NotEqualValues(t, second, third)
+
+	// Make sure out assumption about OTP code rotating every 5 seconds is valid
+	time.Sleep(time.Second * 5)
+
+	fourth, err := vaultUnboxer.Unbox(ctx, otpWithoutCache)
+	require.NoError(t, err)
+
+	require.NotEqualValues(t, third, fourth)
+
+	// Make sure that VAULT_CACHED[...] invocation re-uses the value previously retrieved by VAULT[...] invocation
+	fifth, err := vaultUnboxer.Unbox(ctx, otpWithCache)
+	require.NoError(t, err)
+
+	require.EqualValues(t, fourth, fifth)
 }
