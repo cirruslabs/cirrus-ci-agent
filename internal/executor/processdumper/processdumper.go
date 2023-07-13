93d028690fadfdab1d63d5ba554479b31935c0a5
Nikolay Edigaryev <edigaryev@gmail.com>
2023-07-13 21:06:14 +0400

    processdumper: output process executable path if possible

--- internal/executor/processdumper/processdumper.go
+++ internal/executor/processdumper/processdumper.go
@@ -4,6 +4,7 @@ package processdumper
 
 import (
 	"github.com/mitchellh/go-ps"
+	gopsutilprocess "github.com/shirou/gopsutil/process"
 	"log"
 )
 
@@ -14,7 +15,23 @@ func Dump() {
 	} else {
 		log.Printf("Process list:")
 		for _, process := range processes {
-			log.Printf("%d %d %s", process.Pid(), process.PPid(), process.Executable())
+			log.Printf("%d %d %s", process.Pid(), process.PPid(), processExeOrComm(process))
 		}
 	}
+}
+
+func processExeOrComm(process ps.Process) string {
+	gopsutilProcess, err := gopsutilprocess.NewProcess(int32(process.Pid()))
+	if err != nil {
+		// Fall back to just the comm value
+		return process.Executable()
+	}
+
+	exe, err := gopsutilProcess.Exe()
+	if err != nil {
+		// Fall back to just the comm value
+		return process.Executable()
+	}
+
+	return exe
 }
