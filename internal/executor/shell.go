7c88a9c5fe5f863ac4410d6f417d0b8b351b3cfe
fedor <fedor.korotkov@gmail.com>
2023-11-20 16:03:06 -0500

    Fixed accidental duplicate line

--- internal/executor/shell.go
+++ internal/executor/shell.go
@@ -168,7 +168,6 @@ func NewShellCommands(
 	cmd.Stdout = sc.piper.FileProxy()
 
 	if custom_env != nil {
-	if custom_env != nil {
 		if _, ok := custom_env.Lookup("CIRRUS_AGENT_EXPOSE_SCRIPTS_OUTPUTS"); ok {
 			cmd.Stderr = io.MultiWriter(cmd.Stderr, sc.piper.FileProxy())
 			cmd.Stdout = io.MultiWriter(cmd.Stdout, sc.piper.FileProxy())
